aef8596ba5c8ce36e64a533d21180e3c0b40d8c0
krasimirboyukov77 <krasimirboyukov77@gmail.com>
2024-10-29 20:35:06 +0200

    Added different folders for better project structure. Added IRepository interface and class that implenet the methods.

---
+++ SkillCrest_LearningPlatform.Infrastructure/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillCrest_LearningPlatform.Infrastructure.Repositories.Contracts
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        IEnumerable<T> GetAll();
+
+        Task<T> GetByIdAsync(string id);
+
+        T GetById(string id);
+
+        Task AddAsync(T entity);
+
+        void Add(T entity);
+
+        Task<bool> UpdateAsync(T entity);
+
+        bool Update(T entity);
+
+        Task<bool> DeleteAsync(string id);
+
+        bool Delete(string id);
+    }
+}
